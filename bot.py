313c754686522fddcf9a7cbfe397058cc1e5f0c2
Khiem Vo <8721247+HoloKi@users.noreply.github.com>
2020-03-17 21:29:07 +0100

    Correzione bug e aggiunta Raid in base alla ricognizione immagine

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-from PIL import Image, ImageGrab
+from PIL import Image, ImageGrab, ImageOps
 import os
 import time
 import win32api,win32con
+from numpy import *
 
 #assumo che il gioco sia alla risoluzione base di quando si inizia da 0 
 #metterlo in altro a sinistra in modo che l'angolo del gioco combaci con l'angolo dello schermo 
@@ -12,8 +13,8 @@ y_pad = -1
 def screenGrab():
     box = (x_pad+1,y_pad+1,x_pad+795,y_pad+598)
     im = ImageGrab.grab(box)
-    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
-'.png', 'PNG')
+    #im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +'.png', 'PNG')
+    return im
  
 def main():
     pass
@@ -45,24 +46,35 @@ def get_cords():
     x = x - x_pad
     y = y - y_pad
     print (x,y)
+
+class Cord:
+
+    f_raid = (39,351)
+    f_raidshard = (362, 119)
     
-def startGame():
-    #location del raid
-    mousePos((39,351))
-    leftClick()
-    time.sleep(2)
+def Raid():
+    s = screenGrab()
+    time.sleep(.1)
+    while (s.getpixel(Cord.f_raidshard) != (179, 61, 150)):
+        #location del raid
+        mousePos((39,351))
+        leftClick()
+        time.sleep(2)
      
-    #location di Evoca
-    mousePos((533,425))
-    leftClick()
-    time.sleep(2)
-     
-    #location di Eroico
-    mousePos((584,293))
-    leftClick()
-    time.sleep(2)
-     
-    #location per accettare il team
-    mousePos((511,518))
-    leftClick()
-    time.sleep(2)
+        #location di Evoca
+        mousePos((533,425))
+        leftClick()
+        time.sleep(2)
+         
+        #location di Eroico
+        mousePos((584,293))
+        leftClick()
+        time.sleep(2)
+         
+        #location per accettare il team
+        mousePos((511,518))
+        leftClick()
+        time.sleep(200)
+
+        #accertamento che non ci siano shard
+        s = screenGrab()
