a62e61e2801dd45d81096fae87b90dd1b12da573
Khiem Vo <8721247+HoloKi@users.noreply.github.com>
2021-02-08 20:48:22 +0100

    fix

--- bot.py
+++ bot.py
@@ -1,114 +1,86 @@
-from PIL import Image, ImageGrab, ImageOps
-import os
+import pyautogui
 import time
-import win32api,win32con
-from numpy import *
+from datetime import datetime
 
-#assumo che il gioco sia alla risoluzione base di quando si inizia da 0 
-#metterlo in altro a sinistra in modo che l'angolo del gioco combaci con l'angolo dello schermo 
-#global dichiaration
-x_pad = -1
-y_pad = -1
- 
-def screenGrab():
-    box = (x_pad+1,y_pad+1,x_pad+795,y_pad+598)
-    im = ImageGrab.grab(box)
-    #im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +'.png', 'PNG')
-    return im
 
-def grab():
-    box = (x_pad+1,y_pad+1,x_pad+795,y_pad+598)
-    im = ImageOps.grayscale(ImageGrab.grab(box))
-    a = array(im.getcolors())
-    a = a.sum()
-    print (a)
-    return a
+def log(message,modo):
+    f = open("log.txt",modo)
+    f.write(str(message))
+    f.write("\n")
+    f.close
 
-def get_endraid():
-    box = (243,262,545,366)
-    im = ImageOps.grayscale(ImageGrab.grab(box))
-    a = array(im.getcolors())
-    a = a.sum()
-    print (a)
-    #im.save(os.getcwd() + '\\endraid_one__' + str(int(time.time())) + '.png', 'PNG')    
-    return a
- 
+
+def Raid(run):
+    log("raid","a")
+    time.sleep(1)
+    if(run<1):
+        log("run<1","a")
+        pyautogui.alert(text='the number of raid runs must be 1 or greater',button='OK')
+        exit
+    else:
+        raidcord = pyautogui.locateCenterOnScreen("test1.png",grayscale=False,confidence=0.4)
+        log("run: ","a")
+        log(run,"a")
+        log(raidcord,"a")
+        if raidcord!=None:
+            pyautogui.click(raidcord)
+            time.sleep(1)
+            evoca = pyautogui.locateCenterOnScreen("startraid.png",grayscale=False,confidence=0.5)
+            log("evoca: ","a")
+            log(evoca,"a")
+            if(evoca!=None):
+                pyautogui.click(evoca)
+                time.sleep(1)
+                #if(difficult=='heroic'):
+                difficult = pyautogui.locateCenterOnScreen("eroic.png",grayscale=False,confidence=0.5)
+                log("difficult: ","a")
+                log(difficult,"a")
+                if(difficult!=None):
+                    pyautogui.click(difficult)
+                    time.sleep(1)
+                    accept = pyautogui.locateCenterOnScreen("accept.png",grayscale=False,confidence=0.5)
+                    log("accept: ","a")
+                    log(accept,"a")
+                    if(accept!=None):
+                        pyautogui.click(accept)
+                        #tempo dedicato al completamento del raid in auto
+                        time.sleep(240)
+                        while True:
+                            if(run==1):
+                                yes = pyautogui.locateCenterOnScreen("yes.png",grayscale=False,confidence=0.5)
+                                log("yes: ","a")
+                                log(yes,"a")
+                                time.sleep(1)
+                                pyautogui.click(yes)
+                                break
+                            else:
+                                run=run-1
+                                rerun = pyautogui.locateCenterOnScreen("rerun.png",grayscale=False,confidence=0.5)
+                                log("rerun: ","a")
+                                log(rerun,"a")
+                                time.sleep(1)
+                                pyautogui.click(rerun)
+                                time.sleep(260)
+                    else:
+                        print("AcceptError")
+                        exit
+                else:
+                    print("DifficultError")
+                    exit
+            else:
+                print("EvocaError")
+                exit
+        else:
+            print("RaidError")
+            exit
+
 def main():
-    pass
+    d1 = datetime.now()
+    log(d1,"w+")
+
+print("Start")
+    
+
  
 if __name__ == '__main__':
     main()
-
-def leftClick():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(.1)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-    print ("Click.")
-
-def leftDown():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(.1)
-    print ("left Down")
-         
-def leftUp():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
-    time.sleep(.1)
-    print ("left release")
-
-def mousePos(cord): 
-    win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
-
-def get_cords():
-    x,y = win32api.GetCursorPos()
-    x = x - x_pad
-    y = y - y_pad
-    print (x,y)
-
-class Cord:
-
-    f_raid = (39,351)
-    f_raidshard = (362, 119)
-    f_finishraid = ()
-    
-class Blank:
-    bubble_1 = 2343
-    
-def Raid():
-    s = screenGrab()
-    time.sleep(.1)
-    while (s.getpixel(Cord.f_raidshard) != (179, 61, 150)):
-        s1 = get_endraid()
-        while(s1 != Blank.bubble_1 ):
-            #location del raid
-            mousePos((39,351))
-            leftClick()
-            time.sleep(2)
-         
-            #location di Evoca
-            mousePos((533,425))
-            leftClick()
-            time.sleep(2)
-             
-            #location di Eroico
-            mousePos((584,293))
-            leftClick()
-            time.sleep(2)
-             
-            #location per accettare il team
-            mousePos((511,518))
-            leftClick()
-            time.sleep(1)
-
-            #accertamento che non ci siano shard
-            s = screenGrab()
-            while True:
-                s1 = get_endraid()
-                time.sleep(60)
-                y = s1
-                s2 = get_endraid()
-                if (s2 == y):
-                    break;
-            #location per terminare
-            mousePos((340, 426))
-            leftClick()
-            time.sleep(1)
