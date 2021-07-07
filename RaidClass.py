2b8c55a6b887a18cfd8cba863ced209f78f6c878
Khiem Vo <8721247+HoloKi@users.noreply.github.com>
2021-07-07 23:32:19 +0200

    syntax error fix

--- RaidClass.py
+++ RaidClass.py
@@ -4,12 +4,12 @@ import logging
 import classe
 import sys
 from colorama import *
-from termcolor import colored,cprint
+from termcolor import colored, cprint
 import classe
-init(autoreset=True) #Permette ad ogni print di ritornare al suo colore base
 
+init(autoreset=True)  # Permette ad ogni print di ritornare al suo colore base
 
-errore=colored("Please report this bug/error on github or discord\n",'red',attrs=['bold'])
+errore = colored("Please report this bug/error on github or discord\n", 'red', attrs=['bold'])
 """
 Raid function that uses the class to simplify the code and its reuse
 @: param run = number of runs that the player does (assumes it is already int)
@@ -18,32 +18,34 @@ Raid function that uses the class to simplify the code and its reuse
 @: return 1 if everything went well
 @: return 0 if it went wrong
 """
+
+
 def raid(run, difficult, duration):
     hero = "heroic"
     hard = "hard"
     norm = "normal"
-    difficulty = None #default
-    count=0;
+    difficulty = None  # default
+    count = 0
     # LOAD CLASS FIRST-----------------------------------------------------------------
     if difficult == hero:
-        difficulty = classe.bit(r"image\raid\eroic.png",0.5)
+        difficulty = classe.bit(r"image\raid\eroic.png", 0.5)
     if difficult == hard:
-        difficulty = classe.bit(r"image\raid\hard.png",0.5)
+        difficulty = classe.bit(r"image\raid\hard.png", 0.5)
     if difficult == norm:
-        difficulty = classe.bit(r"image\raid\normal.png",0.5)
+        difficulty = classe.bit(r"image\raid\normal.png", 0.5)
     raid = classe.bit(r"image\raid\raid.png", 0.5)
-    evoca = classe.bit(r"image\startraid.png",0.5)
+    evoca = classe.bit(r"image\startraid.png", 0.5)
     accetta = classe.bit(r"image\accept.png", 0.5)
     si = classe.bit(r"image\yes.png", 0.5)
-    morte = classe.bit(r"image\raid\raiddie.png",0.7)
-    chiudi = classe.bit(r"image\raid\close.png",0.5)
-    #---------------------------------------------------------------------------------
+    morte = classe.bit(r"image\raid\raiddie.png", 0.7)
+    chiudi = classe.bit(r"image\raid\close.png", 0.5)
+    # ---------------------------------------------------------------------------------
     logging.debug(f"difficult = {difficulty}.")
-    print(colored("\n-----RAID-----",'cyan',attrs=['bold']))
-    print(colored("run = ",'green',attrs=['bold']),colored(run,'white'),
-          colored(" difficult = ",'green',attrs=['bold']),colored(difficult,'white'),
-          colored(" and duration = ",'green',attrs=['bold']), colored(duration,'white'),
-          colored("seconds\n",'green',attrs=['bold']))
+    print(colored("\n-----RAID-----", 'cyan', attrs=['bold']))
+    print(colored("run = ", 'green', attrs=['bold']), colored(run, 'white'),
+          colored(" difficult = ", 'green', attrs=['bold']), colored(difficult, 'white'),
+          colored(" and duration = ", 'green', attrs=['bold']), colored(duration, 'white'),
+          colored("seconds\n", 'green', attrs=['bold']))
     logging.debug(f"run = {run}, difficoltà = {difficult}, durata = {duration}")
     time.sleep(3)
     # caso in cui le run sono minori di 0, ritorna 0
@@ -70,22 +72,21 @@ def raid(run, difficult, duration):
             if error == 0:
                 cprint(errore)
                 return 0
-            while(True):
+            while (True):
                 count += 1
                 print("----------------------------------")
                 print(f"run number: {count}")
                 timer(int(duration))
                 print("----------------------------------\n")
-                #controllo se è morto---------------
+                # controllo se è morto---------------
                 error = morte.ispresence()
-                if error == 1: #se è morto clicca
+                if error == 1:  # se è morto clicca
                     chiudi.bottone()
                     print("your team is dead!")
                     time.sleep(5)
                     pyautogui.press('esc')
                     break
-                    return 0
-                #-----------------------------------
+                # -----------------------------------
                 if int(count) >= int(run):
                     logging.debug("end")
                     error = si.bottone()
@@ -93,16 +94,13 @@ def raid(run, difficult, duration):
                         cprint(errore)
                         print("Probably Insufficient time!")
                         break
-                        return 0
                     else:
                         time.sleep(3)
                         pyautogui.press('esc')
                         break
-                        return 1
                 else:
                     pyautogui.press('esc')
                     time.sleep(3)
-
 
 
 def timer(tempo):
@@ -113,4 +111,3 @@ def timer(tempo):
         time.sleep(1)
     sys.stdout.write("\r")
     sys.stdout.write("\033[K")
-
