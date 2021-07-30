dc7db301dfa100af2645544a9e738264ede74140
Khiem Vo <8721247+HoloKi@users.noreply.github.com>
2021-07-31 01:18:28 +0200

    quest click auto

--- ClassBot/DungeonAuto.py
+++ ClassBot/DungeonAuto.py
@@ -36,7 +36,7 @@ def dungeonrepeat(run, difficult):
         else:
             if difficult == norm:
                 difficulty = classe.bit(r"image\raid\normal.png", 0.6)
-
+    quest = classe.bit(r"image\quest.png",0.5)
     dungeon = classe.bit(r"image\dungeon\dungeon.png", 0.5)
     accetta = classe.bit(r"image\accept.png", 0.5)
     si = classe.bit(r"image\yes.png", 0.5)
@@ -49,7 +49,6 @@ def dungeonrepeat(run, difficult):
           colored(" difficult = ", 'green', attrs=['bold']), colored(difficult, 'white'))
     print(" ")
     logging.debug(f"run = {run}, difficoltà = {difficult}")
-    time.sleep(3)
     # caso in cui le run sono minori di 0, ritorna 0
     if int(run) <= 0:
         logging.debug("run < 0")
@@ -61,8 +60,14 @@ def dungeonrepeat(run, difficult):
             logging.debug("run=0")
             return 0
         else:
+            error = quest.bottone()
+            if error == 0:
+                cprint(errore)
+                return 0
             error = dungeon.bottone()
             if error == 0:
+                cprint("Probably something gone wrong with screen")
+                cprint("Please check the wiki or discord server!")
                 cprint(errore)
                 return 0
             error = difficulty.bottone()
