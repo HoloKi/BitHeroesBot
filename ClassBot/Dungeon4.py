9f12cd179a28d0caa22e4e0d5cfa664695e095d9
Khiem Vo <8721247+HoloKi@users.noreply.github.com>
2021-08-30 23:14:05 +0200

    rename

--- ClassBot/Dungeon4.py
+++ ClassBot/Dungeon4.py
@@ -10,48 +10,36 @@ init(autoreset=True)  # Permette ad ogni print di ritornare al suo colore base
 
 errore = colored("Please report this bug/error on github or discord\n", 'red', attrs=['bold'])
 
-
 """
 Dungeon function that uses the class to simplify the code and its reuse
 @: param run = number of runs that the player does (assumes it is already int)
-@: param difficult = level difficulty (assumes it is already a string from the menu)
-@: param duration = time in seconds to complete the run (assumes it is already int)
 @: return 1 if everything went well
 @: return 0 if it went wrong
 """
 
 
-def dungeonrepeat(run, difficult):
-    logging.debug("---------DUNGEON----------")
+def dungeon(run):
+    logging.debug("---------DUNGEON4----------")
     hero = "heroic"
     hard = "hard"
     norm = "normal"
     difficulty = None  # default
     count = 0
     # LOAD CLASS FIRST-----------------------------------------------------------------
-    if difficult == hero:
-        difficulty = classe.bit(r"image\raid\heroic.png", 0.6)
-    else:
-        if difficult == hard:
-            difficulty = classe.bit(r"image\raid\hard.png", 0.6)
-        else:
-            if difficult == norm:
-                difficulty = classe.bit(r"image\raid\normal.png", 0.6)
-    auto = classe.bit(r"image\autored.png", 0.7)
+    auto = classe.bit(r"image\autogreen.png", 0.7)
     quest = classe.bit(r"image\quest.png",0.5)
-    dungeon = classe.bit(r"image\dungeon\dungeon.png", 0.5)
+    dun = classe.bit(r"image\d4\d4.png", 0.5)
+    enter = classe.bit(r"image\entra.png",0.5)
     accetta = classe.bit(r"image\accept.png", 0.5)
-    cittadina = classe.bit(r"image\cittadina.png", 0.5)
+    si = classe.bit(r"image\yes.png", 0.5)
     chiudi = classe.bit(r"image\raid\close.png", 0.5)
     morte = classe.bit(r"image\raid\raiddie.png", 0.7)
     fine = classe.bit(r"image\fine.png", 0.5)
-    # ---------------------------------------------------------------------------------
-    logging.debug(f"difficult = {difficulty.getImage()}.")
-    print(colored("\n-----DUNGEON-----", 'cyan', attrs=['bold']))
-    print(colored("run = ", 'green', attrs=['bold']), colored(run, 'white'),
-          colored(" difficult = ", 'green', attrs=['bold']), colored(difficult, 'white'))
+    # ---------------------------------------------------------------------------------)
+    print(colored("\n-----DUNGEON4-----", 'cyan', attrs=['bold']))
+    print(colored("run = ", 'green', attrs=['bold']), colored(run, 'white'))
     print(" ")
-    logging.debug(f"run = {run}, difficoltà = {difficult}")
+    logging.debug(f"run = {run}")
     # caso in cui le run sono minori di 0, ritorna 0
     if int(run) <= 0:
         logging.debug("run < 0")
@@ -67,13 +55,11 @@ def dungeonrepeat(run, difficult):
             if error == 0:
                 cprint(errore)
                 return 0
-            error = dungeon.bottone()
+            error = dun.bottone()
             if error == 0:
-                cprint("Probably something gone wrong with screen")
-                cprint("Please check the wiki or discord server!")
                 cprint(errore)
                 return 0
-            error = difficulty.bottone()
+            error = enter.bottone()
             if error == 0:
                 cprint(errore)
                 return 0
@@ -82,12 +68,9 @@ def dungeonrepeat(run, difficult):
                 cprint(errore)
                 return 0
             while (True):
-                ''' cause error #see discord support
                 error = auto.ispresence()
                 if error == 0:
-                    logging.debug("auto is not green")
-                    pyautogui.press('space')
-                '''
+                    pyautogui.press("space")
                 count += 1
                 print("----------------------------------")
                 print(f"run number: {count}")
@@ -105,10 +88,9 @@ def dungeonrepeat(run, difficult):
                 print("----------------------------------\n")
                 if int(count) >= int(run):
                     logging.debug("end")
-                    error = cittadina.bottone()
-                    if int(error) == 0:
+                    error = si.bottone()
+                    if error == 0:
                         cprint(errore)
-                        print("Probably Insufficient time!")
                         break
                     else:
                         time.sleep(3)
@@ -120,17 +102,16 @@ def dungeonrepeat(run, difficult):
                         cprint(errore)
                         return 0
                     time.sleep(3)
-        return 0
 
 async def fine():
     fine = classe.bit(r"image\fine.png", 0.7)
     morte = classe.bit(r"image\raid\raiddie.png", 0.7)
     while(True):
         await asyncio.sleep(1)
-        test = morte.SafeControl()
+        test = morte.ispresence()
         if test == 1:
             return 2
-        test = fine.SafeControl()
+        test = fine.ispresence()
         if test == 1:
             return 1
 
